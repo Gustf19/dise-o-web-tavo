9e81830ea6c1ad3c281b11a5477a50b399b19e37
Gustf19 <yetanorts@gmail.com>
2020-08-21 18:33:32 -0500

    Updates

---
+++ precache-manifest.db4d0521605bc39ec92c30bfdb669a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89be002e20c7db0c4caf8dd3defc2bea",
+    "url": "/dise-o-web-tavo/index.html"
+  },
+  {
+    "revision": "1121852eccf44c346c6e",
+    "url": "/dise-o-web-tavo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/dise-o-web-tavo/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dise-o-web-tavo/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1121852eccf44c346c6e",
+    "url": "/dise-o-web-tavo/static/js/main.f66bbb55.chunk.js"
+  },
+  {
+    "revision": "0345204c8ab2cc81b411",
+    "url": "/dise-o-web-tavo/static/js/runtime-main.f478f3b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dise-o-web-tavo/static/media/logo.5d5d9eef.svg"
+  }
+]);
